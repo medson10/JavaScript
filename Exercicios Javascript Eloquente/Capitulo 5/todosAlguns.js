1a330972de944502061f57196e31f75ba196ca84
medson10 <medson.oliveira@fatec.sp.gov.br>
2016-12-24 15:43:34 -0200

    Exercise Todos Alguns added

---
+++ Exercicios Javascript Eloquente/Capitulo 5/todosAlguns.js
@@ -0,0 +1,23 @@
+function every(array, predicate) {
+  for (var cont = 0; cont < array.length; cont++) {
+    if (!predicate(array[cont])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function some(array, predicate) {
+  for (var cont = 0; cont < array.length; cont++) {
+    if (predicate(array[cont])) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+console.log(every([NaN, NaN, NaN], isNaN));
+console.log(every([NaN, NaN, 4], isNaN));
+console.log(some([NaN, 3, 4], isNaN));
+console.log(some([2, 3, 4], isNaN));
