3e111ddb95b56dfdd6c5e2987566c101694f2359
medson10 <medson.oliveira@fatec.sp.gov.br>
2016-12-24 16:20:15 -0200

    Exercise Historico da Expectativa de Vida added

--- Exercicios Javascript Eloquente/Capitulo 5/historicoVida.js
+++ Exercicios Javascript Eloquente/Capitulo 5/historicoVida.js
@@ -1,9 +1,33 @@
 var ancestry = require("./ancestry.js");
 
-var array = ancestry.map(function(Math.ceil(person.died / 100) {
-  return person.died - person.born;
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+function groupBy(array, groupName) {
+  var groups = [];
+  array.forEach(function(person) {
+    var century = groupName(person);
+    if(century in groups) {
+      groups[century].push(person);
+    }
+    else {
+      groups[century] = [];
+      groups[century].push(person);
+    }
+  });
+  return groups;
+}
+
+var arrayCentury = groupBy(ancestry, function(person) {
+  return Math.ceil(person.died / 100);
 });
 
-for (person in array) {
-  console.log(average(array[person]);
+
+for(var century in arrayCentury) {
+  var ages = arrayCentury[century].map(function(person) {
+    return person.died - person.born;
+  });
+  console.log(century + " : " + average(ages));
 }
