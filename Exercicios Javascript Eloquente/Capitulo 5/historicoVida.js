7c9d70b40933285921b26abc693cfa1202343fc8
medson10 <medson.oliveira@fatec.sp.gov.br>
2016-12-23 23:10:21 -0200

    Exercise historico vida added

---
+++ Exercicios Javascript Eloquente/Capitulo 5/historicoVida.js
@@ -0,0 +1,9 @@
+var ancestry = require("./ancestry.js");
+
+var array = ancestry.map(function(Math.ceil(person.died / 100) {
+  return person.died - person.born;
+});
+
+for (person in array) {
+  console.log(average(array[person]);
+}
