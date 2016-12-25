25fa6e44159f0d747faeb9a0a89b26a815895211
medson10 <medson.oliveira@fatec.sp.gov.br>
2016-12-24 22:44:49 -0200

    Exercise Vector added

---
+++ Exercicios Javascript Eloquente/Capitulo 6/vector.js
@@ -0,0 +1,20 @@
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+Object.defineProperty(Vector.prototype, "length", {
+  get: function() { return Math.sqrt(this.x * this.x + this.y * this.y) }
+});
+
+Vector.prototype.plus = function(vector) {
+  return new Vector(this.x + vector.x, this.y + vector.y);
+}
+
+Vector.prototype.minus = function(vector) {
+  return new Vector(this.x - vector.x, this.y - vector.y);
+}
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+console.log(new Vector(3, 4).length);
