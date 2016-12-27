50ba1e6c7dfa247eaf05e6fcb05d724bdff810d3
medson10 <medson.oliveira@fatec.sp.gov.br>
2016-12-27 18:13:13 -0200

    Exercise Regexp Golf added

---
+++ Exercicios Javascript Eloquente/Capitulo 9/regexpGolf.js
@@ -0,0 +1,41 @@
+function verify(regexp, yes, no) {
+// Ignore unfinished tests
+  if (regexp.source == "...") return;
+  yes.forEach(function(s) {
+    if (!regexp.test(s))
+      console.log("Failure to match '" + s + "'");
+  });
+  no.forEach(function(s) {
+    if (regexp.test(s))
+      console.log("Unexpected match for '" + s + "'");
+  });
+}
+
+verify(/ca+(r|t)/,
+    ["my car", "bad cats"],
+    ["camper", "high art"]);
+
+verify(/pr?op/,
+    ["pop culture", "mad props"],
+    ["plop"]);
+
+
+verify(/ferr+(et|y|ari)/,
+    ["ferret", "ferry", "ferrari"],
+    ["ferrum", "transfer A"]);
+
+verify(/ious+\b/,
+    ["how delicious", "spacious room"],
+    ["ruinous", "consciousness"]);
+
+verify(/\s+(\.|:|;|,)/,
+    ["bad punctuation ."],
+    ["escape the dot"]);
+
+verify(/\b[^\s]\w{6,}\b/,
+    ["hottentottententen"],
+    ["no", "hotten totten tenten"]);
+
+verify(/\b[a-df-z]+\b/,
+    ["red platypus", "wobbling nest"],
+    ["earth bed", "learning ape"]);
