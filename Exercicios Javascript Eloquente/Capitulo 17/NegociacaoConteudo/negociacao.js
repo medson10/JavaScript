751524e0f8b937be9864ce881c88c1fc984dfe2d
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-01-02 01:29:18 -0200

    Exercise Negociacao added

---
+++ Exercicios Javascript Eloquente/Capitulo 17/NegociacaoConteudo/negociacao.js
@@ -0,0 +1,18 @@
+function requestAuthor(type) {
+  var req = new XMLHttpRequest();
+  req.open("GET", "http://eloquentjavascript.net/author", false);
+  req.setRequestHeader("accept", type);
+  req.send(null);
+
+  return req.responseText;
+}
+
+var types = ["text/plain", "text/html", "application/json", "application/rainbow+unicorns" ];
+
+types.forEach(function(type) {
+  try {
+    console.log(type + ":\n", requestAuthor(type), "\n");
+  } catch (e) {
+    console.log("Error: " + e);
+  }
+});
