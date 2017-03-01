c93036fd02e094def14f5806bcbc1330eec58bf6
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-02-28 22:21:11 -0300

    Add solution to exercise 4

--- js-best-practices/vendingMachine.js
+++ js-best-practices/vendingMachine.js
@@ -2,14 +2,6 @@ var balanceManager = require('./balanceManager');
 var changeHandler = require('./changeHandler');
 var productInventory = require('./productInventory');
 
-var products = [
-  {
-    name: 'Skittles',
-    price: 85,
-    id: 'A1'
-  }
-];
-
 module.exports = {
 
   insertCoin: function(coinType){
@@ -29,16 +21,6 @@ module.exports = {
     var currentBalance = balanceManager.getBalance();
     balanceManager.decreaseBalance(currentBalance);
     return this.convertToChange(currentBalance);
-  },
-
-
-  getProducts: function() {
-    return products;
-  },
-
-  getProduct: function(productId) {
-    var product = products.find(function(p) { return p.id === productId; });
-    return product;
   },
 
   vendProduct: function(productId){
