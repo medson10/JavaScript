7b7f794a28598ff15363069b8f3a8139a0d2af32
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-02-28 22:14:10 -0300

    Add solution to exercise 3

--- js-best-practices/vendingMachine.js
+++ js-best-practices/vendingMachine.js
@@ -12,30 +12,6 @@ var products = [
 
 module.exports = {
 
-  getAmount: function(coinType) {
-    // COINS:
-    // [p]enny
-    // [n]ickel
-    // [d]ime
-    // [q]uarter
-    switch(coinType){
-      case 'p': return 1;
-      case 'n': return 5;
-      case 'd': return 10;
-      case 'q': return 25;
-      default: throw new Error('Unrecognized coin ' + coinType);
-    }
-  },
-
-  getProducts: function() {
-    return products;
-  },
-
-  getProduct: function(productId) {
-    var product = products.find(function(p) { return p.id === productId; });
-    return product;
-  },
-
   insertCoin: function(coinType){
     var value = balanceManager.getAmount(coinType);
     balanceManager.increaseBalance(value);
@@ -53,6 +29,16 @@ module.exports = {
     var currentBalance = balanceManager.getBalance();
     balanceManager.decreaseBalance(currentBalance);
     return this.convertToChange(currentBalance);
+  },
+
+
+  getProducts: function() {
+    return products;
+  },
+
+  getProduct: function(productId) {
+    var product = products.find(function(p) { return p.id === productId; });
+    return product;
   },
 
   vendProduct: function(productId){
