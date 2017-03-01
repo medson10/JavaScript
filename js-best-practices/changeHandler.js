8270269570c14380efe430a335c88d6dbce4bd9b
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-03-01 17:41:28 -0300

    Add solution to exercise 5

--- js-best-practices/changeHandler.js
+++ js-best-practices/changeHandler.js
@@ -1,18 +1,17 @@
-module.exports = {
+var coins = {
+     'p': 1,
+     'n': 5,
+     'd': 10,
+     'q': 25
+   };
 
+var coinsByAmount = ['q', 'd', 'n', 'p'];
+
+module.exports = {
   getAmount: function(coinType) {
-    // COINS:
-    // [p]enny
-    // [n]ickel
-    // [d]ime
-    // [q]uarter
-    switch(coinType){
-      case 'p': return 1;
-      case 'n': return 5;
-      case 'd': return 10;
-      case 'q': return 25;
-      default: throw new Error('Unrecognized coin ' + coinType);
+    if(!coins.hasOwnProperty(coinType)){
+      throw new Error('Unrecognized coin ' + coinType);
     }
+    return coins[coinType];
   }
-  
 };
