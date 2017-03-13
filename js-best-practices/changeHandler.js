f756049d25dddefe1816d23107f9c39bf1e9e2ad
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-03-13 12:24:31 -0300

    Add solution to exercise 6

--- js-best-practices/changeHandler.js
+++ js-best-practices/changeHandler.js
@@ -13,5 +13,49 @@ module.exports = {
       throw new Error('Unrecognized coin ' + coinType);
     }
     return coins[coinType];
+  },
+  // Initial solution
+
+  // convertToChange: function(valor) {
+  //   var change = [];
+  //   if(valor >= coins['q']) {
+  //     for(var i = 1; i <= Math.floor(valor / coins['q']); i++) {
+  //       change.push('q');
+  //     }
+  //     valor = valor % coins['q'];
+  //   }
+  //   if(valor >= coins['d']) {
+  //     for(var i = 1; i <= Math.floor(valor / coins['d']); i++) {
+  //       change.push('d');
+  //     }
+  //     valor = valor % coins['d'];
+  //   }
+  //   if(valor >= coins['n']) {
+  //     for(var i = 1; i <= Math.floor(valor / coins['n']); i++) {
+  //       change.push('n');
+  //     }
+  //     valor = valor % coins['n'];
+  //   }
+  //   if(valor >= coins['p']) {
+  //     for(var i = 1; i <= Math.floor(valor / coins['p']); i++) {
+  //       change.push('p');
+  //     }
+  //     valor = valor % coins['p'];
+  //   }
+  //   return change;
+  // }
+
+  //Official solution
+  convertToChange: function(amount) {
+      var change = [];
+      for(var i in coinsByAmount){
+        var coinType = coinsByAmount[i];
+        var coinValue = coins[coinType];
+        while(amount >= coinValue){
+          change.push(coinType);
+          amount -= coinValue;
+        }
+      }
+    return change;
   }
 };
